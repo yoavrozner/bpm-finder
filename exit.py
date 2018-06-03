0848db6088440ab2c5dc6082f5364d8a5b790735
yoavrozner <rozner.yoav@gmail.com>
2018-06-03 18:18:26 +0300

    Final with doc

--- exit.py
+++ exit.py
@@ -1,44 +1,46 @@
 # -*- coding: utf-8 -*- 
 
-###########################################################################
-## Python code generated with wxFormBuilder (version Jun 17 2015)
-## http://www.wxformbuilder.org/
-##
-## PLEASE DO "NOT" EDIT THIS FILE!
-###########################################################################
+"""
+Name: Yoav Rozner
+Version: 1.16
+Description: Subprocess of the Bpm Finder which opens a window
+             to exit the song or the metronome.
+"""
 
 import wx
 import wx.xrc
 
 ###########################################################################
-## Class MyPanel1
+## Class BpmFinderExit
 ###########################################################################
 
 
 class BpmFinderExit(wx.Frame):
+    """
+    Subprocess exit gui.
+    """
     def __init__(self, parent):
         wx.Frame.__init__(self, parent, id=wx.ID_ANY, pos=wx.DefaultPosition, size=wx.Size(169, 113),
                           style=wx.TAB_TRAVERSAL)
 
         self.SetBackgroundColour(wx.Colour(128, 255, 0))
 
-        bSizer1 = wx.BoxSizer(wx.VERTICAL)
+        box_sizer1 = wx.BoxSizer(wx.VERTICAL)
 
         self.exit = wx.Button(self, wx.ID_ANY, u"Exit", wx.DefaultPosition, wx.Size(200, 400), 0)
-        bSizer1.Add(self.exit, 0, wx.ALL | wx.EXPAND | wx.ALIGN_CENTER_HORIZONTAL, 5)
+        box_sizer1.Add(self.exit, 0, wx.ALL | wx.EXPAND | wx.ALIGN_CENTER_HORIZONTAL, 5)
 
-        self.SetSizer(bSizer1)
+        self.SetSizer(box_sizer1)
         self.Layout()
 
         # Connect Events
         self.exit.Bind(wx.EVT_BUTTON, self.exit_song)
 
-    def __del__(self):
-        pass
-
-
-    # Virtual event handlers, overide them in your derived class
+    # Virtual event handlers, override them in your derived class
     def exit_song(self, event):
+        """
+        Closes the subprocess on button click.
+        """
         raise SystemExit
 
 
